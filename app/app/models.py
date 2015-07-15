9227e209de7180beb5ec0b9e46e54f18ff89c38b
geodotka <dorota.kowalik@21s.pl>
2015-07-16 00:15:21 +0200

    Create basic models and initial migration

---
+++ app/app/models.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from django.db import models
+
+
+class Concrete(models.Model):
+    # beton
+    type = models.CharField(max_length=20, verbose_name='klasa')
+
+
+class Steel(models.Model):
+    # stal
+    type = models.CharField(max_length=20, verbose_name='klasa')
+
+
+class Diametersw(models.Model):
+    # średnica in mm
+    value = models.IntegerField(verbose_name='wartość')
+
+
+class Support(models.Model):
+    # podpora
+    type = models.CharField(max_length=30, verbose_name='typ')
+
+
+class Sect(models.Model):
+    # przekrój
+    type = models.CharField(max_length=20, verbose_name='typ')
+
+
+class Dsit(models.Model):
+    # sytuacja obliczeniowa
+    type = models.CharField(max_length=20, verbose_name='typ')
+
+
+class Punching(models.Model):
+    # materials
+    c_class = models.ForeignKey(Concrete)
+    s_class = models.ForeignKey(Steel)
+    dsw = models.ForeignKey(Diametersw)
+
+    # geometry
+    support = models.ForeignKey(Support)
+    sect = models.ForeignKey(Sect)
+    b = models.DecimalField(max_digits=3, decimal_places=1)
+    h = models.DecimalField(max_digits=3, decimal_places=1)
+    dx = models.DecimalField(max_digits=3, decimal_places=1)
+    dy = models.DecimalField(max_digits=3, decimal_places=1)
+    lx = models.DecimalField(max_digits=2, decimal_places=1)
+    ly = models.DecimalField(max_digits=2, decimal_places=1)
+    ad = models.DecimalField(max_digits=2, decimal_places=1)
+    lambda_u = models.IntegerField()
+    asx = models.DecimalField(max_digits=3, decimal_places=1)
+    asy = models.DecimalField(max_digits=3, decimal_places=1)
+
+    # work conditions
+    dsit = models.ForeignKey(Dsit)
+    ved = models.DecimalField(max_digits=4, decimal_places=1)
+    beta = models.DecimalField(max_digits=3, decimal_places=2)
+
+    #results
+    vrdc = models.DecimalField(
+        max_digits=4, decimal_places=3, null=True, blank=True)
+    vrdmax = models.DecimalField(
+        max_digits=4, decimal_places=3, null=True, blank=True)
