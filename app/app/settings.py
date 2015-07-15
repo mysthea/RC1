6131d702f0bed4b8bbe342192b14dec172bdd8cf
geodotka <dorota.kowalik@21s.pl>
2015-07-16 00:14:41 +0200

    Set language settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,14 +79,10 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'pl-pl'
+TIME_ZONE = 'Europe/Warsaw'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
