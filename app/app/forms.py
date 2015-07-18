46e81485c01213a26a4ee67351d5df834067b9ca
geodotka <dorota.kowalik@21s.pl>
2015-07-18 19:15:55 +0200

    #2: set initial values in choice fields in punching form

--- app/app/forms.py
+++ app/app/forms.py
@@ -5,6 +5,15 @@ from django import forms
 
 from .models import Punching
 
+INITIAL_VALUE_CHOICE_DICT = {
+    'c_class': 'C20/25',
+    's_class': 'RB500',
+    'dsw': '8',
+    'support': 'słup wewnętrzny',
+    'sect': 'prostokątny',
+    'dsit': 'trwała',
+}
+
 
 class PunchingForm(forms.ModelForm):
 
@@ -12,3 +21,13 @@ class PunchingForm(forms.ModelForm):
         model = Punching
         exclude = ['vrdc', 'vrdmax']
 
+    def __init__(self, *args, **kwargs):
+        super(PunchingForm, self).__init__(*args, **kwargs)
+
+        for k, v in INITIAL_VALUE_CHOICE_DICT.items():
+            field = self.fields.get(k)
+            initial_index = next(
+                [choice[0] for choice in field.choices
+                 if choice[1] == v].__iter__(), None)
+            if initial_index:
+                self.initial[k] = str(initial_index)
